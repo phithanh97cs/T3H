1bd8070b0a2bc03e240e482fee765f6855cc0d4b
huypn <huypn@Laptop086.nttdata.com.vn>
2018-06-08 08:20:10 +0700

    add slide show

--- Front-end/website/js/myjs.js
+++ Front-end/website/js/myjs.js
@@ -66,7 +66,7 @@ function stickyNavigator() {
 // effect to button "back to top"
 function backToTopButton() {
 	if ($(this).scrollTop() >= 50) { // If page is scrolled more than
-											// 50px
+		// 50px
 		$('#return-to-top').fadeIn(200); // Fade in the arrow
 	} else {
 		$('#return-to-top').fadeOut(200); // Else fade out the arrow
@@ -81,5 +81,40 @@ function setUpBackToTopAction() {
 		// Scroll to top of body
 		}, 500);
 	});
-
 }
+
+/*FOR SLIDE SHOW START**/
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+//Next/previous controls
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	var dots = document.getElementsByClassName("dot");
+	if (n > slides.length) {
+		slideIndex = 1
+	}
+	if (n < 1) {
+		slideIndex = slides.length
+	}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slides[slideIndex - 1].style.display = "block";
+	dots[slideIndex - 1].className += " active";
+}
+/*FOR SLIDE SHOW END**/
