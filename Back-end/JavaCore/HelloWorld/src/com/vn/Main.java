0d9005e2ca698c7e08fd9197f5e8a447e40609d4
huypn <huypn@Laptop086.nttdata.com.vn>
2018-06-20 08:35:26 +0700

    add new code

--- Back-end/JavaCore/HelloWorld/src/com/vn/Main.java
+++ Back-end/JavaCore/HelloWorld/src/com/vn/Main.java
@@ -4,13 +4,15 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		
-		while(true) {
-			Scanner sc = new Scanner(System.in);
-			String name = sc.nextLine();
-			System.out.println("Hello: " + name);
-			if (name.equals("exit") ) {
-				sc.close();
+		Scanner sc = new Scanner(System.in);
+		while(true) {  //Sử dụng vòng lặp cho việc nhập không giới hạn
+			
+			String name = sc.nextLine(); // Đọc dòng dữ liệu nhập
+			System.out.println("Hello: " + name); // Hiển thị dữ liệu nhập
+			if ("exit".equals(name) ) { // nẾU nhập exit thì dừng việc nhập không giới hạn
+				break;
 			}
 		}
+		sc.close(); // Đóng lắng nghe từ input
 	}
 }
