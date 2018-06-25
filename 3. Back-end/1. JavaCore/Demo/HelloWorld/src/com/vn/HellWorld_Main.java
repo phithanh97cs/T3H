07228304686ef9988e01631055ecdac7b7ab9445
huypn <huypn@Laptop086.nttdata.com.vn>
2018-06-25 08:46:00 +0700

    rename

---
+++ 3. Back-end/1. JavaCore/Demo/HelloWorld/src/com/vn/HellWorld_Main.java
@@ -0,0 +1,19 @@
+package com.vn;
+
+import java.util.Scanner;
+
+public class HellWorld_Main {
+	
+
+	public static void main(String[] args) {
+		//cach 1
+		System.out.println("Hello World");
+		
+		Scanner scanner;
+		scanner = new Scanner(System.in);
+		System.out.println("Nhập tên bạn: ");
+		String name = scanner.nextLine();
+		System.out.println("Tên bạn vừa nhập: "+ name);
+	}
+	
+}
