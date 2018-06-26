3649fec869c90ec6af935e5f6708288a77ddc426
huypn <huypn@Laptop086.nttdata.com.vn>
2018-06-26 16:01:55 +0700

    add vehicle

--- 3. Back-end/1. JavaCore/Demo/HelloWorld/src/com/javacore/basic/entity/Vehicle.java
+++ 3. Back-end/1. JavaCore/Demo/HelloWorld/src/com/javacore/basic/entity/Vehicle.java
@@ -2,20 +2,19 @@ package com.javacore.basic.entity;
 
 import com.javacore.basic.interfaces.VehicleInterfacce;
 
-public class Car extends Vehicle implements VehicleInterfacce {
-	int cadence = 0;
-	int speed = 0;
-	int gear = 1;
-	
+public class Vehicle implements VehicleInterfacce {
+	public int wheel;
+	public float speed;
+
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	@Override
 	public void start() {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 }
