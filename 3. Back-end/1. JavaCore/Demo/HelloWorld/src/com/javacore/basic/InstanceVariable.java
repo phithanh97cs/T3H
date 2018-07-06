2168ea4adc548e468f24256c4bf99bde95474edd
HuyPN <HuyPN@192.168.1.68>
2018-07-06 07:19:45 +0700

    add

--- 3. Back-end/1. JavaCore/Demo/HelloWorld/src/com/javacore/basic/InstanceVariable.java
+++ 3. Back-end/1. JavaCore/Demo/HelloWorld/src/com/javacore/basic/InstanceVariable.java
@@ -1,10 +1,25 @@
 package com.javacore.basic;
 
-import com.javacore.basic.entity.Car;
-
 public class InstanceVariable {
 	public static void main(String[] args) {
-		Car car = new Car();
-		System.out.println(car.speed); // =>> gọi trực tiếp
+		
+		int[] x = {1, 2, 3, 4, 5, 6, 7 ,8 ,9};
+		
+//		for (int u = 9;true; ) {
+//			u++;
+//			if (u > 13) {
+//				break;
+//			}
+//			System.out.println(u);
+//		}
+		for (int i : x) {
+			if (i == 5) {
+				break;
+			}
+			System.out.println(i);
+		}
+		
+		
+		
 	}
 }
